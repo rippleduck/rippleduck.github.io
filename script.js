062242bcaea09a87509b0b7eefef6a9b50fec2db
rippleduck <34221633+rippleduck@users.noreply.github.com>
2017-12-04 08:19:07 +0800

    changes

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ function getkorean()
 }
 
 
-setInterval( getkorean(),500);
+setInterval( getkorean(),22200);
 
  //********** this one gets the  price of XRP on the api EVERY 200 MILISECONDS
 setInterval( 
